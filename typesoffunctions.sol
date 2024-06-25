c4003c7885e80e3c975776997f27f5af843756a0
nikita sharma <150901367+nikitaxarma@users.noreply.github.com>
2024-06-25 23:09:22 +0530

    Update typesoffunctions.sol

--- typesoffunctions.sol
+++ typesoffunctions.sol
@@ -1,25 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20 {
+contract MyToken is ERC20, Ownable {
 
-    constructor() ERC20("MyToken", "MTK") {}
+    constructor() ERC20("MyToken", "MTK") Ownable(msg.sender) {}
 
-    // Function to mint tokens (only callable by contract owner)
-    function mint(address to, uint256 amount) external {
+    // Function to mint tokens
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    // Function to burn tokens
-    function burn(uint256 amount) external {
+    // Function for users to burn their own tokens
+    function burn(uint256 amount) public {
         _burn(msg.sender, amount);
     }
 
-    // Function to transfer tokens
-    function transfer(address to, uint256 amount) public override returns (bool) {
-        _transfer(_msgSender(), to, amount);
-        return true;
-    }
+    // Transfer function is already part of the ERC20 standard
 }
+//0x583031D1113aD414F02576BD6afaBfb302140225
